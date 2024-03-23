440c2c3d1035573d4701dd366d8dc5f476e1bb49
Tausifqureshi <tausifqureshi504@gmail.com>
2024-03-23 23:48:02 +0530

    Operator-Arithmetic

--- oparetor.js
+++ oparetor.js
@@ -1,5 +1,8 @@
 // ================================================= JS Operator ===============================================//
-//JS me itne operators hote hai ?
+
+//whats is the operator ---> js me jab ham kisi value ya variable me store ki value par jis special symbol ka use kar ke oppression parform karte hai us se operator bolte hai ? Javascript operators are used to perform different types of mathematical and logical computations.
+
+//============================JS me itne operators hote hai =====================================?
 
 // Arithmetic Operators
 // Assignment Operators
@@ -11,10 +14,21 @@
 // Type Operators
 
 
+// ==========================================Arithmetic Operators ==============================================//
+// Arithmetic consept ---> 
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Remainder)
+// ++	Increment
+// --	Decrement
 
-
-
-
+let a = 100;
+let b = 50;
+let x = a + b; //share Arihtmetic operatoe aise hi use kar sakte hai ?
+console.log(x); 
 
 
 
