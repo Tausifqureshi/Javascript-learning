b46228fb172006ceea8fb24aa522f20df9dc3625
Tausifqureshi <tausifqureshi504@gmail.com>
2024-03-24 12:34:01 +0530

    Operator-Assingment

--- oparetor.js
+++ oparetor.js
@@ -19,18 +19,46 @@
 // +	Addition
 // -	Subtraction
 // *	Multiplication
-// **	Exponentiation (ES2016)
+// **	Exponentiation (ES2016) //ES 2016 me aya ye verions javascript ?
 // /	Division
 // %	Modulus (Remainder)
 // ++	Increment
 // --	Decrement
 
-let a = 100;
+let a = 6;
 let b = 50;
-let x = a + b; //share Arihtmetic operatoe aise hi use kar sakte hai ?
-console.log(x); 
+
+// let x = a + b; //share Arihtmetic operatoe aise hi use kar sakte hai ?
+// let x = a ** 2; //ES 2016 me aya ye verions javascript ?
+// console.log(x); 
+
+//console.log(b++);//Increment aise karne se nhi pata chale ga 1 add hoya console.log(b) tab malum hogi b ki value ?
+// console.log(b);
+// console.log(++b); //Increment 1 add hoga is me ?
+
+console.log(--b); // Decrement 1 kam hoga is se mynus
 
 
+
+// ======================================== Assignment Operators ==============================================//
+// Assignment Operators ---> ye o operator hai jis ki help se kisi variable me new value ko Assign kiya jata hai ?
+
+
+// =	Assignment Operators
+// +=	 Assignment Addition Operators ?
+// -=	 Assignment  Subtraction Operators ?
+// *=	 Assignment Multiplication Operators ?
+// /=	 Assignment Division  Operators ?
+// %=	 Assignment Modulus (Remainder)  Operators ?
+// **=  Assignment Exponentiation Operators (ES2016) //ES 2016 me aya ye verions javascript ?
+
+// let c = 10;
+// let z = c += 5;  //is se hora hai c me jo puri value ti us c me ham fir 5 += assign kar re hai is ka answer 15 hoga ? sortcut bolte hai is ko ?
+// console.log(z) // sub same hi operator aise use karna hai ?
+
+// let c = 6;
+// let z = c **= 2; //Assignment Exponentiation Operators (ES2016) //ES 2016 me aya ye verions javascript ?
+// console.log(z);
 
 
 
