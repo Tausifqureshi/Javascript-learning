3aa654b8dc7fef51aee23365d2181c0c39ed13a6
Tausifqureshi <tausifqureshi504@gmail.com>
2024-03-30 14:52:05 +0530

    File

--- Constructor.js
+++ Constructor.js
@@ -1,5 +1,5 @@
 //=========================================JS Function Object Constructor=======================================//
-//Is se hi ham Constructor function ya Constructor object dono same hi hai ? Constructor used kar ke Main object se new object bana sakte hai or new object ko Multipal time alag alag object property ke sath print kar sakte hai ? Jab bhi ham Constructor Function bante hai us ka First letter Captital hota hai ? ek se jayda bar obejct ko copy na kar re is liye ham object Constructor banaya gaya hai Javascript me ? Ye bhi Object oriented programming me hi ata hai ?
+//Is se hi ham Constructor function ya Constructor object dono same hi hai ? Constructor used kar ke Main object se new object bana sakte hai or new object ko Multipal time alag alag object property ke sath print kar sakte hai ? Jab bhi ham object Constructor Function bante hai us ka First letter Captital hota hai ? ek se jayda bar obejct ko copy na kar re is liye ham object Constructor banaya gaya hai Javascript me ? Ye bhi Object oriented programming me hi ata hai ?
 
 
 // =========================================== Glowvia Teac Chaneal Chaptert =================================//
@@ -16,30 +16,31 @@
 
 
 
-//Creating Using ObjectConstructor ?
-function Car (name, modal, color,weight){ //Constructor function me first letter Captal hota hai ?
-  this.carName = name, //this means currnet object ka referance means Car object ka referace ?
-  this.carModa = modal,
-  this.carColor = color,
-  this.carWeight = weight
-};
+//Creating Using Object-Constructor ?
+// function Car (name, modal, color,weight){ //Object-Constructor function me first letter Captal hota hai ?
+//   this.carName = name, //this means currnet object ka referance means Car object ka referace ?
+//   this.carModa = modal,
+//   this.carColor = color,
+//   this.carWeight = weight
+// };
 
-let car1 = new Car ('Tata Nexon',2023, "Red", '800 kg'); //Constructor function ko call karne ka tarika hai new keyword ?
-console.log(car1.carName);  // is tara se koi bhi value print karwa sakte carModal jo bhi print karwna ho ?
+// let car1 = new Car ('Tata Nexon',2023, "Red", '800 kg'); //Constructor function ko call karne ka tarika hai new keyword ?
+// // console.log(car1.carName);  // is tara se koi bhi value print karwa sakte carModal jo bhi print karwna ho ?
 
-car1.speed = 150 ; //is tara se proprty add kar sakte hai ?
+// car1.speed = 150 ; //is tara se proprty add kar sakte hai ?
 
 // delete car1.speed ; //aise delete bhi kar sakte hai property  ko ?
 
-console.log(car1)  //pura Car name ka Function aye ga is se ?
+// console.log(car1)  //pura Car name ka Function aye ga is se ?
 
 // let car2 = new Car ('Tata Nexon',2023, "orange", '800 kg'); //kitne bhi Car object bana sakte hai aise ?
 // console.log(car2);
 
 
 // let car3 = new Car ('Tata Nexon',2023, "Yellow", '800 kg');
+// console.log(car3); //Pura object print hopga is se
 
-// car3.carInfo = function(){  //kisi bhi object me method add karne ka tarika ?
+// car3.carInfo = function(){  //kisi bhi object me method add karne ka tarika abhi ye car3-object me add hora hai ?
 //   return this.carName + " " + this.carModa ;
 // }
 
@@ -48,10 +49,21 @@ console.log(car1)  //pura Car name ka Function aye ga is se ?
 // console.log(car3); // check methpd  add hua ya nhi car3 variable me ? 
 
 
+// ================================== Object-Constructor-Prototype ================================//
+// Object-Constructor-prototype ---> is ko ham Object-Prototype bhi bolte hai ? 
 
 
+function Car (name, modal, color,weight){ //Object-Constructor function me first letter Captal hota hai ?
+  this.carName = name, //this means currnet object ka referance means Car object ka referace ?
+  this.carModa = modal,
+  this.carColor = color,
+  this.carWeight = weight
+};
 
+let car1 = new Car ('Tata Nexon',2023, "Red", '800 kg'); //Constructor function ko call karne ka tarika hai new keyword ?
+// console.log(car1.carName);  // is tara se koi bhi value print karwa sakte carModal jo bhi print karwna ho ?
 
+console.log(car1)  //pura Car name ka Function aye ga is se ?
 
 
 
@@ -66,7 +78,7 @@ console.log(car1)  //pura Car name ka Function aye ga is se ?
 // ==================================Tech Gun Chanel ka Capter ================================//
 // 
 
-// function Show (name,id,selery,){ // Constructor Shoe ka first capital is se bolte hai Constructor function ?
+// function Show (name,id,selery,){ // Constructor Show ka first capital is se bolte hai Constructor function ?
 //     this.name = name;   //this matlab us object ka refer lena
 //     this.id= id;
 //     this.selery= selery;
@@ -77,8 +89,10 @@ console.log(car1)  //pura Car name ka Function aye ga is se ?
 //  }
 // Show.prototype.nationlity = 'indian'; //constructor me alag se add karne ka tarika ye aisa
 
-//let add = new Show("Tausif",12,2300); //new keyword se constructor Object create hora show wale function ka?
+//let add = new Show("Tausif",12,2300); //new keyword se constructor Object create hora show wale function ka? 
+
 //add.nationlity= 'indian'  //only object me add add hoga aise constructor me add nhi hoga ?
+
 
 //add.hello = function(){  //add object me ek method bhi add hora hai hello name ka means function?
 // return this.name + "" + this.id
