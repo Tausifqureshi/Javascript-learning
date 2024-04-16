a774278d6ee87511e53d30f64e37b49e5e38165e
Tausifqureshi <tausifqureshi504@gmail.com>
2024-04-16 18:39:20 +0530

    Json-Parse

--- json.js
+++ json.js
@@ -3,217 +3,28 @@
 // JSON me Data type itne store kar sakte hai ?
 // String,Array,Null,Boolean,Number,Object ?
 
-let dtails ={
-    name: "Tausif",                                   //String value
-    "age" :24,                                         //Number Value
-    // "married" : false ,                               //Boolean value
-
-    // student ={                                      //Object value
-    //  "name": "Sofiya","age":23,"City": "Kodad" },
-
-    // "coruse": ["HTML","CSS","Javascrip"],              //Array value
-    // "kids": ,                                          //Null value
-
-    // "city": "Mumbai"    
-// }
-
-}
-
-console.log(dtails)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// JSON.parse ---> server se jab bhi koi data ata hai o string me hota hai us se object me convart karne ke liye hi JSON.Parse ka use hota hai ?
+ 
+// JSON.stringify ---> server pe jab bhi object me jo bhi data jata hai us data ko string me change karta hai ye method ?
+
+let details = {
+  firstName: "Tausif", //String value
+  age: 24, //Number Value
+  married: false, //Boolean value
+
+  car: { //nested object 
+    name: "Tata", 
+    modal: 2024,
+    color: "gray",
+  },
+
+  coruse: ["HTML","CSS","Javascrip"],              //Array value
+  kids: null,                                         //Null value
+  city: "Mumbai"
+  
+};
+
+console.log(details);
+
+// console.log(typeof details.kids); //dost use kar ke bhi Print karwa sakte hai ?
+// console.log(details["firstName"]); //aise bhi access kar sakte breaket notation se jo bhi properry lena ho ?
