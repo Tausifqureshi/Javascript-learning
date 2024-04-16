100d246609833eea399392a665badd187c9ba09c
Tausifqureshi <tausifqureshi504@gmail.com>
2024-04-16 19:08:43 +0530

    Json-Parse

--- json.js
+++ json.js
@@ -4,27 +4,48 @@
 // String,Array,Null,Boolean,Number,Object ?
 
 // JSON.parse ---> server se jab bhi koi data ata hai o string me hota hai us se object me convart karne ke liye hi JSON.Parse ka use hota hai ?
- 
+
 // JSON.stringify ---> server pe jab bhi object me jo bhi data jata hai us data ko string me change karta hai ye method ?
 
-let details = {
-  firstName: "Tausif", //String value
-  age: 24, //Number Value
-  married: false, //Boolean value
+let details = `{
+ "firstName": "Tausif", 
+ "age": 24,
+ "married": false, 
+ "coruse": ["HTML","CSS","Javascrip"],             
+ "kids": null,                                         
+  "city": "Mumbai",
+ "car":{ 
+    "name": "Tata", 
+    "modal": 2024,
+    "color": "gray"
+   }
 
-  car: { //nested object 
-    name: "Tata", 
-    modal: 2024,
-    color: "gray",
-  },
+}`;
 
-  coruse: ["HTML","CSS","Javascrip"],              //Array value
-  kids: null,                                         //Null value
-  city: "Mumbai"
-  
-};
+let jsonParse = JSON.parse(details); //server se data object me convert hoke aye ga json.parse se ?
+console.log(jsonParse);
 
-console.log(details);
+// console.log(details);
 
 // console.log(typeof details.kids); //dost use kar ke bhi Print karwa sakte hai ?
 // console.log(details["firstName"]); //aise bhi access kar sakte breaket notation se jo bhi properry lena ho ?
+
+
+
+let details1 = {
+    firstName: "Tausif", 
+    age: 24,
+    married: false, 
+    coruse: ["HTML","CSS","Javascrip"],             
+    kids: null,                                         
+     city: "Mumbai",
+    car:{ 
+       name: "Tata", 
+       modal: 2024,
+       color: "gray"
+      }
+   
+   };
+
+let jsonStringfy = JSON.stringify(details1); //server pe data bejne ke liye use hota hai json.stringfy ?
+console.log(jsonStringfy)
