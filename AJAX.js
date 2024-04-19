bc782321d7ce6661db63f06aa95ae2030380dbff
Tausifqureshi <tausifqureshi504@gmail.com>
2024-04-20 01:38:46 +0530

    E-Commerce

--- AJAX.js
+++ AJAX.js
@@ -1,5 +1,8 @@
 // AJAX ---> Is ka Full-Form hai  Asynchronous JavaScript And XML.? AJAX koi programming language nhi hai ye ek set of technologies hai Asynchronous JavaScript And XML ? AJAX me Page reloading kar re bina page update kar sakte hai mains koi button ko upadete karna hai us ka page refresh kar re bina upadate kar sate hai ? Modern Website JSON ka use karti hai na ke XML---> is ka full-orm hoota hai Extensible Markup Language ? 
 
+console.log("AJAX Tutorial");
+
+
 
 
 
