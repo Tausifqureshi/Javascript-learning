1e44de7844f2dd20bc52fe318c9989ac425edb79
Tausifqureshi <tausifqureshi504@gmail.com>
2024-04-21 23:59:15 +0530

    File Attch

--- AJAX.js
+++ AJAX.js
@@ -1,7 +1,17 @@
-// AJAX ---> Is ka Full-Form hai  Asynchronous JavaScript And XML.? AJAX koi programming language nhi hai ye ek set of technologies hai Asynchronous JavaScript And XML ? AJAX me Page reloading kar re bina page update kar sakte hai mains koi button ko upadete karna hai us ka page refresh kar re bina upadate kar sate hai ? Modern Website JSON ka use karti hai na ke XML---> is ka full-orm hoota hai Extensible Markup Language ? 
+// AJAX ---> Is ka Full-Form hai  Asynchronous JavaScript And XML.? AJAX koi programming language nhi hai ye ek set of technologies hai Asynchronous JavaScript And XML ? AJAX me Page reloading kar re bina page update kar sakte hai mains koi button ko upadete karna hai us ka page refresh kar re bina upadate kar sate hai ? AJAX ka use kar ke ham fast and dynamic web page bana sakta hai ? Modern Website JSON ka use karti hai na ke XML---> is ka full-orm hoota hai Extensible Markup Language ? 
 
 console.log("AJAX Tutorial");
 
+async function myFun() {
+  try {
+    let call = await fetch("data2.json");
+    let respon = await call.json();
+    console.log(respon);
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+  }
+}
+myFun()
 
 
 
