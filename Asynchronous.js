0fee8204edb9681b8de35098a870e5da3cc2ee2c
Tausifqureshi <tausifqureshi504@gmail.com>
2024-04-21 10:17:18 +0530

    Asynchronous

--- Asynchronous.js
+++ Asynchronous.js
@@ -11,7 +11,7 @@
 // Asynchronous ---->  ek sath hamne 4 request send ki browser pe jo request ka respons hame jaldi mile ga us se bolte hai Asynchronous Programming ? Asynchronous use jayda tar promies me karte hai ham ? Asynchronous is ka means multiplr kam ek sath kar sakte hai ? Asynchronous jo Asynchronous  nhi hai us se block nhi kar ta hai ye ?
 
 console.log("Front-ent Developer");
-setTimeout(() => {  // Asynchronous hai ye code is se 1 second lage ga jab tak 'Front-ent Developer' and  "Full-stact Developer" code execute ho jaye ge ek second ke baad last me setTime wala code run hoga ?
+setTimeout(() => {  // Asynchronous hai ye code is se 1 second lage ga jab tak 'Front-ent Developer' and  "Full-stact Developer" code execute ho jaye ge ek second ke baad last me setTime wala code run hoga is se bolte hai Asynchronous Progrmming ?
 
     console.log("Web Developer");
 }, 2000);
