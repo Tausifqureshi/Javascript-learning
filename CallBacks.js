df3f09d70d1451fccd6004134013a16e47313eeb
Tausifqureshi <tausifqureshi504@gmail.com>
2024-04-30 02:55:08 +0530

    Call-Back-Hell

--- CallBacks.js
+++ CallBacks.js
@@ -19,7 +19,7 @@
 // let valueSave = sum1(1,2,calculator1);
 // console.log(valueSave);
 
-
+//============================= Example 1
 // function calculator() {
 //     console.log('Calculator Function Called');
 //     // return 'Calculator has finished execution';
@@ -39,13 +39,42 @@
 
 
 
+
+
 // ========================================= Call-Back Hell =======================================//
 
+// Call-Back Hell ----> function jab nested ho jate hai us se hi Call-Back Hell bolte hai ?  jab Call-Back Hell ho jata hai o pyramid of mood dehkta hai ? javascript me Call-Back Hell problem tab karte hai jab ek se jayda Call-Back ek dusre ke ander nested ho jate hai une mainten and manage karne me issu ata hai ? Call-Back Hell ko avoided karna chaciye us ki jaga promise ya Async/await use karna chaiye ?
 
 
+function gerRollNo() {  //is tara shep ko pyramid of doom bolte hai is avoided Call-Back Hell ?
+    console.log("Start The Code Executon")
+    setTimeout(()=>{
+        console.log('API getting roll no');
+        let roll_no = [1,2,3,4,5];
+        console.log(roll_no);
 
+        setTimeout((rollnumber) => {
+            console.log("Access the name and roll-number");
+            let biodata ={
+                name: "Tausif",
+                age: 25,
+            }
+            console.log(`My name is ${biodata.name} and I an ${biodata.age} years old My ${rollnumber} Number`)
+            // console.log(rollnumber);
 
+            setTimeout((name) => {
+                biodata.gender = "Male";
+                console.log(`My name is ${biodata.name} Gender is ${biodata.gender}`);
+                // console.log(name);
 
+            }, 2000,biodata.name);
+
+        },2000,roll_no[1]); //2 milge ga us array ki index se accessing kar re hai ?
+
+    }, 2000)
+}
+
+gerRollNo()
 
 
 
