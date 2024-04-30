9aba4eb257ba57c7b4b1bb3df65704aaa2459d4b
Tausifqureshi <tausifqureshi504@gmail.com>
2024-04-30 13:28:50 +0530

    Promise

--- promise.js
+++ promise.js
@@ -6,9 +6,9 @@
 
 //  promise two property ko support karta hai state and reuslt ?
 // myPromise.state	        myPromise.result
-// "pending"	            undefined
+// "pending"	              undefined
 // "fulfilled"	            a result value
-// "rejected"	            an error object
+// "rejected"	              an error object
 
 
 // promise property ?
@@ -21,54 +21,38 @@
 // Rejected ?
 // When a Promise object is "rejected", the result is an error object.
 
-console.log('First of the Code execute');
-setTimeout(() => {
-  console.log("SetTimeout Code Execute distance 3 mili second");
-}, 3000);
-console.log("After the Code Execute");
-
-
 
 
 
 let AccessButton = document.getElementById("ajax");
-// AccessButton.addEventListener("click", myPromise);
+AccessButton.addEventListener("click", myPromise);
 
-// function myPromise(){
-// console.log('Clicked');
+function myPromise(){
+console.log('Clicked');
 
-// let myPromise = new Promise(function(myResolve, myReject) {
-//   let x = 8;
+let myPromise = new Promise(function(myResolve, myReject) {
+  let x = 7;
     
-//     if (x == 8) {
-//       myResolve("OK");
-//     } else {
-//       myReject("Error");
-//     }
-//   });
-// //   console.log(typeof myPromise) //promise bhi ek object hai ?
-//   myPromise.then(
-//     function(value) {
-//     console.log(value);
-//     },
-//     function(error) {
-//     console.log(error);
-//     }
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (x == 8) {
+      myResolve("OK");
+    } else {
+      myReject(new Error("I am a Error"));
+    }
+  });
+//   console.log(typeof myPromise) //promise bhi ek object hai ?
+  myPromise.then(
+    function(value) {
+    console.log(value);
+    },
+    // function(error) { //ye line commit tab jake catch wala code execute hoga ?
+    // console.log(error);
+    // }
+  )
+  .catch (function(error){
+    console.log('Caught by catch:', error);
+  })
+}
+console.log('Tasif')
 
 
 
