1efae931ac188ed6bcf5ec8b1918017dd0a38419
Tausifqureshi <tausifqureshi504@gmail.com>
2024-05-22 12:51:32 +0530

    promise-Initinal

--- object-oriented2.js
+++ object-oriented2.js
@@ -12,9 +12,14 @@ function SetUserName(userName){
     this.myName = userName
   }
   
-  
-  function CreateUser(userName,email,password) {
-    SetUserName(userName)
+
+function CreateUser(userName,email,password) {
+SetUserName.call(this,userName); //---> ham is wale function se userName set karwana hai ? is me jo ham userName pass kar re hai o is hi function CreateUser ka hi jo ham ek argument dere hai q ke hamne setUserName me ek Parameter pass kiya hai tu argument bhi dena hoga is liye ham argument ke taur per is Createuser function me ka userName de re hai ? ham is function ka use kar ke CraeteUser fubction wale ka userName Set kar re hai ? call ham is liye use kar re hai Call function ka use kar ke ek object ki property dusre object use kar sakta haiu ? this object ko refere karta hai is SetUserName() me jo this hai o CreateUser function ke userName ke liye call hora hai or console me is ki propertymyName ke set hogi q ke hame SetUserName se username set kar re hai tu hamne is usename varibel ka naam ya key bol do is naam myName rahka hai is liye CreateUser function wale userName me jo bhi value aye gi is myName me set ho jaye gi ?
+
+
+
+    // this.myName = userName; ----> //aisa bhi set kar sakte hai but hame abhi aisa nhi karna hame ek dusre function ki value ko use karna hai is wale function me ?
+
     this.myEmail = email;
     this.myPassword = password;
   }
