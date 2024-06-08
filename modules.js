290d27ceecda2dc622bab0b8b4d370a27f5bd580
Tausifqureshi <tausifqureshi504@gmail.com>
2024-06-09 00:35:55 +0530

    Modules

--- modules.js
+++ modules.js
@@ -1,6 +1,16 @@
 // Modules js
 
-// ChatGPT Se liya hua Syntax --->
+// ChatGPT Se liya hua Syntax ---> JavaScript mein modules ka istemal kar ke aap apne code ko organize kar sakte hain, reusable components bana sakte hain, aur code ko maintain karne mein madad mil sakti hai. Modules basically code ko alag-alag files mein organize karne ka ek tareeqa hai, jisse aapko code ko easily manage karne mein madad milti hai.
+
+// ChatGPT Se liya hua Syntax ---> JavaScript mein modules ke prakar
+// Script Modules: Script modules ko <script> tag ke sath import kiya jaata hai. Ye sabse basic type ka module hai.
+
+// CommonJS Modules: CommonJS modules ko require() function ke sath import kiya jaata hai. Ye Node.js mein sabse aam type ka module hai.
+
+// ES Modules (ESM): ES Modules ko import keyword ke sath import kiya jaata hai. Ye browser mein sabse aam type ka module hai.
+
+
+
 
 import config from "./modules1.js"; // Default import aisa karte hai.
 
