90814b268f0ec8d243a63bcc08af28158e7ed6aa
Tausifqureshi <tausifqureshi504@gmail.com>
2024-06-29 14:59:34 +0530

    Memory file updated

--- hosting.js
+++ hosting.js
@@ -1,23 +1,31 @@
-// hoisting  --> mans function hosting support karata hai us se first upper call karo ya niche o support karta hai only function declaretion function Expreeson nhi supporet karta hai ? but ham  variable create kar ke us ke upper consloe karte hai tu variable bhi hosting support nhi karta hai? javascript me class bhi Hosting Support nhi leta hai ?
+// ChatGPT Se liya hua Syntax ---> hoisting  
 
-// hello();
 
-// function hello(){
-//     console.log("hello world");
-// }
-// console.log(hello);
-// const hello = "hello world";
-// console.log(hello);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 
 //  javascript me class bhi Hosting Support nhi leta hai ?
-
 // let emoloyes2 = new Employes ('2','Qureshi');  // 
 // emoloyes2.details(); //emoloyes2 ko first value dena? bad me  Employes class initialization karna is se bolte hai hosting ? class bhi Hosting Support nhi leta hai
-
 
 class Employes{ //Cretae Class //This Chaptert learnVarn
     constructor(id,name){  //Create Object constructor use kar ke?
@@ -25,11 +33,16 @@ class Employes{ //Cretae Class //This Chaptert learnVarn
         this.name = name
     }
    details(){   //Sort Method Means object ke ander function creating ?
-    document.writeln(`${this.id} ${this.name}`)
-    console.log(`${this.id} ${this.name}`)
+    // document.writeln(`${this.id} ${this.name}`)
+    // console.log(`${this.id} ${this.name}`)
+    return (`${this.id} ${this.name}`)
    }
 }
 
 let emoloyes1 = new Employes ('1','Tausif');  // new keyword se constructor object ke argumnet hai means value ?
-emoloyes1.details();
+let emoloyes2 = new Employes ('2','qureshi');  // new keyword se constructor object ke argumnet hai means value ?
+ let store = emoloyes2.details();
+ console.log(store)
+
+
 
