7d28a5f3e557e75bdf2e2916e64488d98db7433e
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-23 09:44:15 +0530

    JavaScript

--- OOPS/Object-Oriented2.js
+++ OOPS/Object-Oriented2.js
@@ -6,7 +6,7 @@
 
 // ChatGPT Se liya hua Syntax ---> OOP ka new Concept hai jo ES6 (ECMAScript 2015) me aya hai javacsript me ab is se hi jayda tar use karte hai class ko ?
 
-//Constructo ===== ChatGPT Se liya hua Syntax ---> Constructor ek special method hota hai jo object ko initialize karta hai. Isme initial properties set ki jati hain.
+//Constructor ===== ChatGPT Se liya hua Syntax ---> Constructor ek special method hota hai jo object ko initialize karta hai. Isme initial properties set ki jati hain.
 
 // OOPs Concept in JavaScript ---> is se interveiw me  pucte hi puchte hai ---> is ke 4 Piller hote hai  Abstraction, Encapsulation,Inheritance,Polymorphism ?
 
