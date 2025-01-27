ffff6880ece9d57baf982015a6e9c97179f60067
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-27 23:53:13 +0530

    JS

--- Function-Chapter/higher-order-function.js
+++ Function-Chapter/higher-order-function.js
@@ -40,12 +40,12 @@ function first(firstParametar) {
   function third(callBack, callbacktwo) {
     callBack("Tausif");
     callbacktwo("Qureshi"); //jab ham argument me value dere hai funcion me tu ham first and second wale function me parameter bhi lena hoga.
-  rst, s
+ 
     // callBack(argument1);
     // callbacktwo(argument2);
   }
   
-  third(fiecond);
+  third(first, second);
   // third(first,second ,"Tausif","Qureshi"); //callBack Function jo third wale function me call hore hai us me jo argument deta hai fir hame us argument ki value bhi dere hai Tausif Qureshi.
   
   
